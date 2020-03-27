937e16fc2fe155fc88f5da7a41489f9471443db2
Haider Ali <haiderali0118@gmail.com>
2020-03-28 00:10:36 +0500

    error handling

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const express = require('express');
+const morgan = require('morgan')
 const app = express();
 
 const productsRoute = require('./api/routes/products')
 const ordersRoute = require('./api/routes/orders')
 
-// app.use('/',(req,res,next) => {
-//     res.status(200).json({
-//         message: "Server has started at port 5000!"
-// })
-// })
+app.use(morgan('dev'));
+
 app.use('/products',productsRoute);
 app.use('/orders',ordersRoute);
+
+app.use((req,res,next) =>{
+    const error = new Error("Found an error");
+    error.status = 404;
+    next(error);
+})
+
+app.use((error,req,res,next) =>{
+    res.status(error.status || 500).json({
+        error :{
+            message: error.message
+        }
+    })
+})
 
 module.exports = app;
