511a9664c9cc3c3a5140f01db80cbf6838f48092
Haider Ali <haiderali0118@gmail.com>
2020-03-28 00:53:39 +0500

    cors

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const morgan = require('morgan')
+const morgan = require('morgan');
+const cors = require('cors');
 const app = express();
 
-const productsRoute = require('./api/routes/products')
-const ordersRoute = require('./api/routes/orders')
+const productsRoute = require('./api/routes/products');
+const ordersRoute = require('./api/routes/orders');
 
 app.use(morgan('dev'));
+app.use(cors());
 
 app.use('/products',productsRoute);
 app.use('/orders',ordersRoute);
@@ -17,7 +19,9 @@ app.use((req,res,next) =>{
 })
 
 app.use((error,req,res,next) =>{
-    res.status(error.status || 500).json({
+    res
+    .status(error.status || 500)
+    .json({
         error :{
             message: error.message
         }
